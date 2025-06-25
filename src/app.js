32c06e3f37160ff3cc757d306a9c8a87782267ab
gocode901 <210020119@iitb.ac.in>
2025-06-25 17:57:05 +0530

    use env properly

--- src/app.js
+++ src/app.js
@@ -14,29 +14,11 @@ app.use(cookieParser())
 
 // routes import
 import userRouter from "./routes/user.route.js"
-app.listen(8000)
 
 // routes declaration
-// app.use("/users", userRouter)
 app.use("/api/v1/users",userRouter)
-
 // https://localhost:8000/api/v1/users/registerUser
 
+export  default app;
 
-export {app} ;
-
-
-// // test.js
-// import express from 'express';
-// const app = express();
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World');
-// });
-
-// app.listen(8000, '0.0.0.0', () => {
-//   console.log("Test server running on 8000");
-// });
-
-// export default app
 
--- src/index.js
+++ src/index.js
@@ -1,34 +1,23 @@
 // require('dotenv').config({ path: "./env"})
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
-import {app} from "./app.js"
+import app from "./app.js"
 
+dotenv.config({ path: "./.env"})
 // -r dotenv/config --experimental-json-modules
 // we need to add this in package.json/ nodemon 
 
-dotenv.config({
-  path: "./.env"
-})
 
 connectDB()
 .then(() => {
-  app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server is running at port : ${process.env.PORT}`);
-  })
+  app.listen(process.env.PORT || 8000, () => 
+    console.log(`Server is running at port : ${process.env.PORT||8000}`)
+  )   
 })
 .catch((err) => {
   console.log("MONGO db connection failed !!! ", err);
   process.exit(1);
 })
-
-
-// function connectDB() {}
-// connectDB();      //calling the function immediately
-
-// or
-
-// (function connectDB(){})();
-// or
 
 // ********
 // this is the "iife(immediately invoked function expression)" approach where we have the connection code itself in the index file and other approach will be writing the connection code separately in db file
