87d42ccd5801c5755288465f69e35ac5772a7339
gocode901 <210020119@iitb.ac.in>
2025-06-27 17:30:40 +0530

    add controllers and routes for comments, likes, playlists, subscriptions, tweets, videos, and healthcheck functionality

--- src/app.js
+++ src/app.js
@@ -14,11 +14,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-// routes import
-import userRouter from "./routes/user.route.js";
+// import routes
+import userRouter from './routes/user.route.js'
+import healthcheckRouter from "./routes/healthcheck.route.js"
+import tweetRouter from "./routes/tweet.route.js"
+import subscriptionRouter from "./routes/subscription.route.js"
+import videoRouter from "./routes/video.route.js"
+import commentRouter from "./routes/comment.route.js"
+import likeRouter from "./routes/like.route.js"
+import playlistRouter from "./routes/playlist.route.js"
+import dashboardRouter from "./routes/dashboard.route.js"
 
-// routes declaration
-app.use("/api/v1/users", userRouter);
+//routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 // https://localhost:8000/api/v1/users/registerUser
 
 export default app;
